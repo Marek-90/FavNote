45880ff9596287f255d47e92640b486046c47815
Marek Sieńkowski <mareksienkowski@MacBook-Pro-Marek.local>
2022-01-15 15:07:01 +0100

    styled

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -4,10 +4,12 @@ const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap');
 *, *::before, *::after {
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 html {
-    font-size: 62.5%;
+    font-size: 62.5%; 
 }
 
 body {
--- src/view/Root.js
+++ src/view/Root.js
@@ -1,8 +1,10 @@
 import Button from "../components/Button/Button";
+import GlobalStyle from "../theme/GlobalStyle";
 
 const Root = () => {
   return (
     <>
+      <GlobalStyle />
       <h1>Hello Marek</h1>
       <Button>close / save</Button>
       <Button secondary>Remove</Button>
